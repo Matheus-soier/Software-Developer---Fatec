7612971d55073e7b72c1057222417e637331e461
Matheus Sampaio Soier <132232562+Matheus-soier@users.noreply.github.com>
2023-08-19 16:08:09 -0300

    updates

---
+++ Java/ProgramaImc/src/progImc.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class progImc {
+    public static void main(String[] args) throws Exception {
+        Scanner calculoImc = new Scanner(System.in);
+
+        System.out.println("Digite sua altura: ");
+        float fltAltura = calculoImc.nextFloat();
+        System.out.println("Digite seu peso: ");
+        float fltPeso = calculoImc.nextFloat();
+
+        float resultadoImc = fltPeso / (fltAltura * fltAltura);
+
+        if (resultadoImc >= 18.5 && resultadoImc <= 25) {
+            System.out.println("Você está no peso ideal, seu IMC é de: " + resultadoImc);
+        } else {
+            System.out.println("Você não está no peso ideal, seu IMC é de: " + resultadoImc);
+        }
+        calculoImc.close();
+    }
+}
