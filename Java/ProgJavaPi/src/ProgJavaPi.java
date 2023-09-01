ab237e8453501f36cd1c12caf9b67d37236b799b
Matheus Sampaio Soier <132232562+Matheus-soier@users.noreply.github.com>
2023-09-01 17:44:33 -0300

    updates

---
+++ Java/ProgJavaPi/src/ProgJavaPi.java
@@ -0,0 +1,37 @@
+// import java.util.Scanner;
+
+public class ProgJavaPi {
+    public static void main(String[] args) throws Exception {
+        // String firstName, lastName, email, userLogin, cep, cpf;
+        // double passwordLogin;
+        // char sexo;
+        // int telefone, idade;
+
+        // Scanner LoginForm = new Scanner(System.in);
+
+        // System.out.println("----------------------------");
+        // System.out.println("---FORMULÁRIO DE CADASTRO---");
+        // System.out.println("----------------------------");
+
+        // System.out.println("Digite seu Primeiro Nome: ");
+        // firstName = LoginForm.nextLine();
+        // System.out.println("Digite seu Ultimo Nome: ");
+        // lastName = LoginForm.nextLine();
+        // System.out.println("Digite sua Idade: ");
+        // idade = LoginForm.nextInt();
+        // System.out.println("Digite seu CPF: ");
+        // cpf = LoginForm.nextLine();
+        // System.out.println("Digite seu Sexo: (F para Feminino / M para Masculino)");
+        // sexo = LoginForm.nextLine().charAt(0);
+        // System.out.println("Digite seu telefone para Contato: (DDD + Número)");
+        // telefone = LoginForm.nextInt();
+        // System.out.println("Digite seu CEP: ");
+        // cep = LoginForm.nextLine();
+
+        // System.out.println("----------------------------");
+        // System.out.println("---CRIE SEU LOGIN DE ACESSO---");
+        // System.out.println("----------------------------");
+
+        // LoginForm.close();
+    }
+}
