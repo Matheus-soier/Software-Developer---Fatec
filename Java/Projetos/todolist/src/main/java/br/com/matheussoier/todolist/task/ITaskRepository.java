acee563545e78f0f572a5f3d4d89bff347552ec3
Matheus Sampaio Soier <devmatheus.soier@gmail.com>
2023-10-11 14:12:41 -0300

    Updates ToDoList Spring Boot

---
+++ Java/Projetos/todolist/src/main/java/br/com/matheussoier/todolist/task/ITaskRepository.java
@@ -0,0 +1,9 @@
+package br.com.matheussoier.todolist.task;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ITaskRepository extends JpaRepository<TaskModel, UUID>{
+    
+}
