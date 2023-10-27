c7a8489b5261af1445cb28c31ea761d566d537ae
Matheus Sampaio Soier <devmatheus.soier@gmail.com>
2023-10-27 19:00:41 -0300

    updates java - encapsulamento

--- Java/Projetos/SistemaBancario/src/Conta.java
+++ Java/Projetos/SistemaBancario/src/Conta.java
@@ -1,7 +1,11 @@
-public class Conta {
-    int agencia;
-    int numero;
-    double saldo;
+import java.io.Serializable;
+
+public class Conta implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    private int agencia;
+    private int numero;
+    private double saldo;
 
     public Conta(){}
     public Conta(int agencia, int numero, double saldo) {
@@ -15,7 +19,24 @@ public class Conta {
     public void sacar(double valor) {
         this.saldo-=valor;
     }
-    public double verificarSaldo(){
+    public double getSaldo(){
         return saldo;
     }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(int agencia) {
+        this.agencia = agencia;
+    }
+
 }
--- Java/Projetos/SistemaBancario/src/ContaTeste.java
+++ Java/Projetos/SistemaBancario/src/ContaTeste.java
@@ -1,17 +1,13 @@
 public class ContaTeste {
     public static void main(String[] args) {
         Conta contaCorrente = new Conta();
-        Conta contaPoupança = new Conta(111, 222, 200);
+       contaCorrente.setAgencia(123);
+       contaCorrente.setNumero(321);
+       contaCorrente.depositar(500);
 
-        contaCorrente.agencia = 321;
-        contaCorrente.numero = 123;
-        contaCorrente.saldo = 150;
-
-        contaCorrente.depositar(1000);
-        System.out.println("Saldo conta corrente: " + contaCorrente.verificarSaldo());
-
-        contaPoupança.sacar(25);
-        System.out.println("Saldo conta poupança: " + contaPoupança.verificarSaldo());
-
+    contaCorrente.sacar(200);
+        System.out.println(contaCorrente.getSaldo());
     }
+
+    Conta contaCorrente = new Conta(123,321,1000);
 }
